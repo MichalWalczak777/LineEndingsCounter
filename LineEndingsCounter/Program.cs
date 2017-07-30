b4816edd9d40bfe893ddd4f9efa8d44be5948966
Michal Walczak <michalwalczak777@gmail.com>
2017-07-30 02:35:28 +0200

    Class ExampleFileCreator has been added.

--- LineEndingsCounter/Program.cs
+++ LineEndingsCounter/Program.cs
@@ -12,10 +12,16 @@ namespace LineEndingsCounter
         {
 
             FileCounter ds = new FileCounter();
+            ExampleFilesCreator fileCreator = new ExampleFilesCreator();
 
             string path = System.Environment.CurrentDirectory;
             path = Directory.GetParent(Directory.GetParent(path).ToString()).ToString();
             path = path + "\\Endings";
+
+            fileCreator.CreateCRLFEndingsFiles(path, "subfolder1", 3);
+            fileCreator.CreateLFEndingsFiles(path, "subfolder1", 2);
+            fileCreator.CreateLFEndingsFiles(path, "", 1);
+            fileCreator.CreateMixedEndingsFiles(path, "subfolder2", 4);
 
             ds.CountAndDisplay(path);
         }
